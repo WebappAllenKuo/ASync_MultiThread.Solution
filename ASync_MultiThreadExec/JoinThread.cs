ba0af3c64a4de4d44f2f1f7bb99df0e19419f1ac
Allen Kuo <g01.webapp@gmail.com>
2021-03-12 20:00:05 +0800

    add JoinThread.cs

---
+++ ASync_MultiThreadExec/JoinThread.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace ASync_MultiThreadExec
+{
+    public class JoinThread
+    {
+
+        [Test]
+        public void Test()
+        {
+            Thread t1 = new Thread(DoJob);
+            Thread t2 = new Thread(DoJob);
+            Thread t3 = new Thread(DoJob);
+            
+            t1.Start("X");
+            t2.Start("Y");
+            t3.Start("Z");
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.Write(".");
+            }
+            
+            // t1.Join();
+            // t2.Join();
+            // t3.Join();
+            
+           // 沒有寫join(),所以下一行程式有可能比上面三個thread還要早執行
+            Console.WriteLine("[XXXXXXXXXXXXXXXX]");
+        }
+
+        public void DoJob(object name)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.Write(name + " ");
+                // Thread.Sleep(10);
+            }
+        }
+    }
+}
