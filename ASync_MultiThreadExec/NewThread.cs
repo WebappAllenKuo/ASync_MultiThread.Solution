1f24c1f516c5386ab78dbe2be7f0f00e26df8bd8
Allen Kuo <g01.webapp@gmail.com>
2021-03-12 19:42:44 +0800

    add NewThread.cs

---
+++ ASync_MultiThreadExec/NewThread.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace ASync_MultiThreadExec
+{
+    public class NewThread
+    {
+        [Test]
+        public void Test()
+        {
+            Thread t = new Thread(DoSomething);
+            t.Start();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(".");
+            }
+            
+        }
+
+        public void DoSomething()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write(i + "\t");
+                // Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("\n");
+        }
+    }
+}
