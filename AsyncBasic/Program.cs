e326c1ab40fb30f0ed91e802d67bd089ce57764e
LillianLin <a888gogogo@yahoo.com.tw>
2021-03-14 07:31:21 +0800

    add AsyncBasic project,同步執行的程式碼

---
+++ AsyncBasic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+
+namespace AsyncBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://www.webapp.com.tw/";
+            string content = MyDownloadPage(url);
+
+            Console.WriteLine($"一共 {content.Length} 個字元");
+
+        }
+
+        static string MyDownloadPage(string url)
+        {
+            var client = new WebClient();
+            string content = client.DownloadString(url);
+            return content;
+        }
+    }
+}
