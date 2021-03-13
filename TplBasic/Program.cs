18bfb4d1f4d23a12205fbee43924eabc4267a818
LillianLin <a888gogogo@yahoo.com.tw>
2021-03-14 07:03:19 +0800

    t1.Wait(); // 會丟到task完全結束，才再向下執行

---
+++ TplBasic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TplBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = new Task(MyTask); // TPL是在thread pool 執行，所以是背景thread,主程式結束，它就被迫結束
+            t1.Start();
+            t1.Wait(); // 會丟到task完全結束，才再向下執行
+            for (int i = 0; i < 500; i++)
+            {
+                Console.Write(".");
+                // Thread.Sleep(100);
+            }
+        }
+
+        static void MyTask()
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                Console.Write($"[{Thread.CurrentThread.ManagedThreadId}]");
+                //Thread.Sleep(1);
+            }
+        }
+    }
+}
