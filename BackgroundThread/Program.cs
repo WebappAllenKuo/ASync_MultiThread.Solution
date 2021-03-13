15e2b0683cba914ba27b5e4ef2baeeb94dd4abf0
LillianLin <a888gogogo@yahoo.com.tw>
2021-03-14 05:45:31 +0800

    若設為背景thread, 只會執行一下下，就會因為主thread結束而不再執行背景thread

---
+++ BackgroundThread/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace BackgroundThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = new Thread(MyTask); 
+            t1.IsBackground = true; // 若設為背景thread, 只會執行一下下，就會因為主thread結束而不再執行背景thread
+            t1.Start();
+        }
+
+        static void MyTask()
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
