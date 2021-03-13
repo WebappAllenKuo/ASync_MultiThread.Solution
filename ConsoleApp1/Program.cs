88233b8332976133d0e19fdb87be6fd7f235ce41
LillianLin <a888gogogo@yahoo.com.tw>
2021-03-14 06:06:46 +0800

    add ConsoleApp1 project, 由於它是background thread,所以主程式結束，背景thread就自動結束，不再執行，可以看到[N]的字沒有很多

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(MyTask); // 用 Thread Pool 來建立多執行緒，它是背景thread
+            for (int i = 0; i < 500; i++)
+            {
+                Console.Write(".");
+            }
+        }
+
+        static void MyTask(object state)
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                Console.Write($"[{Thread.CurrentThread.ManagedThreadId}]");
+                Thread.Sleep(10);
+            }
+            
+        }
+    }
+}
