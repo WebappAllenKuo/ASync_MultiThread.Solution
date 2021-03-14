0ceb3b64dcf6a959ae7ca127a2daf56e36574e53
LillianLin <a888gogogo@yahoo.com.tw>
2021-03-14 19:58:09 +0800

    完成非同步的程式碼

--- ThreadLog/Program.cs
+++ ThreadLog/Program.cs
@@ -1,13 +1,38 @@
 ﻿using System;
+using System.Net;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ThreadLog
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            
+            var task = MyDownloadPageAsync("http://www.webapp.com.tw/");
+
+            string content = await task;
+
+            Console.WriteLine($"總共內容 {content.Length} 字元");
         }
-        
+
+        static void Log(int num, string msg)
+        {
+            Console.WriteLine($"<{num}> T{Thread.CurrentThread.ManagedThreadId}: {msg}");
+        }
+
+        static async Task<string> MyDownloadPageAsync(string url)
+        {
+
+            using (var wc = new WebClient())
+            {
+                var task = wc.DownloadStringTaskAsync(url);
+
+                string content = await task;
+
+                return content;
+            }
+        }
     }
 }
