dfb8c36c9c6641282d5c721cf3d01d5e26b7cc06
LillianLin <a888gogogo@yahoo.com.tw>
2021-03-14 19:52:49 +0800

    ...

--- AsyncBasic/Program.cs
+++ AsyncBasic/Program.cs
@@ -1,18 +1,35 @@
 ﻿using System;
 using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsyncBasic
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            string url = "http://www.webapp.com.tw/";
-            Task<string> content = MyDownloadPageAsync(url);
+            // string url = "http://www.webapp.com.tw/";
+            // Task<string> content = MyDownloadPageAsync(url);
+            //
+            // Console.WriteLine($"一共 {content.Result.Length} 個字元");
 
-            Console.WriteLine($"一共 {content.Result.Length} 個字元");
+            int num1 = 10, num2 = 30;
+            int sum = await MyTask(num1, num2);
 
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(".");
+            }
+            
+            Console.WriteLine(sum);
+
+        }
+
+        static async Task<int> MyTask(int num1, int num2)
+        {
+            Thread.Sleep(5000);
+            return num1 + num2;
         }
 
         static async Task<string> MyDownloadPageAsync(string url)
