321520d2ddb2254418bfe5d476c035f2f8ec18d3
LillianLin <a888gogogo@yahoo.com.tw>
2021-03-14 20:02:48 +0800

    用log學習 thread 資訊

--- ThreadLog/Program.cs
+++ ThreadLog/Program.cs
@@ -10,10 +10,16 @@ namespace ThreadLog
     {
         static async Task Main(string[] args)
         {
+            Log(1,"正要起始非同步 MyDownloadPageAsync.");
+            
             var task = MyDownloadPageAsync("http://www.webapp.com.tw/");
 
+            Log(4,"從 MyDownloadPageAsync 返回，但目前尚未取得工作結果");
+            
             string content = await task;
 
+            Log(6,"已經取得 MyDownloadPageAsync 結果");
+            
             Console.WriteLine($"總共內容 {content.Length} 字元");
         }
 
@@ -24,13 +30,18 @@ namespace ThreadLog
 
         static async Task<string> MyDownloadPageAsync(string url)
         {
-
+            Log(2, "MyDownloadPageAsync Start.");
+            
             using (var wc = new WebClient())
             {
                 var task = wc.DownloadStringTaskAsync(url);
-
+                
+                Log(3,"wc.DownloadStringTaskAsync start.");
+                
                 string content = await task;
 
+                Log(5,"已經取得網頁內容");
+                
                 return content;
             }
         }
