60d58194da6a65e0373549d2e7e212264aead4cc
LillianLin <a888gogogo@yahoo.com.tw>
2021-03-14 21:53:51 +0800

    test2 task 不同傳回型別，但又要WhenAll的寫法

--- TaskWhenAll/Program.cs
+++ TaskWhenAll/Program.cs
@@ -10,27 +10,23 @@ namespace TaskWhenAll
         {
             https://stackoverflow.com/questions/17197699/awaiting-multiple-tasks-with-different-results
 
-            Task<int> t1 = MyTask1(); // 這裡會卡 6秒才再向下
-            Task<string> t2 = MyTask2(); //這裡會卡 10秒才再向下
+            Task<int> t1 = MyTask1(); // 這裡不會卡
+            Task<string> t2 = MyTask2(); //這裡不會卡
 
-            await Task.WhenAll(t1, t2); // 由於上二行都完成，所以不會卡住，直接向下
-            int num = await t1;
-            string value = await t2;
+            await Task.WhenAll(t1, t2); // 這裡會卡10秒，等二個都完成，才再向下
+            int num = await t1; //由於已完成，所以這裡可以直接取值，不會卡
+            string value = await t2;//由於已完成，所以這裡可以直接取值，不會卡
             
-            // foreach (var result in results)
-            // {
-            //     Console.WriteLine(result);
-            // }
         }
 
         static async Task<int> MyTask1()
-        {
-            Thread.Sleep(6000);
+        { 
+            await Task.Delay(6000);
             return 1;
         }
         static async Task<string> MyTask2()
         {
-            Thread.Sleep(10000);
+            await Task.Delay(10000);
             return "allen";
         }
         
