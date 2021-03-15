309bc086f550e0850ebde2c0be63aedabe11e9c6
LillianLin <a888gogogo@yahoo.com.tw>
2021-03-15 14:50:27 +0800

    modify AsyncExec project,同步程式完成

--- AsyncExec/Program.cs
+++ AsyncExec/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
 
 namespace AsyncExec
 {
@@ -6,7 +9,26 @@ namespace AsyncExec
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Log("Main start.");
+
+            string url = "http://www.google.com/";
+            DownloadString(url);
+            
+            Log("Main end.");
+        }
+
+        static void DownloadString(string url)
+        {
+            using (var client =new WebClient())
+            {
+                string content = client.DownloadString(url);
+                Log($"DownloadString/content has {content.Length} chars.");
+            }
+        }
+        static void Log( string msg,int? seqNum=null)
+        {
+            string numTtext = seqNum.HasValue ? seqNum.Value.ToString() : string.Empty;
+            Console.WriteLine($"{numTtext}\tT[{Thread.CurrentThread.ManagedThreadId}] {msg}");
         }
     }
 }
