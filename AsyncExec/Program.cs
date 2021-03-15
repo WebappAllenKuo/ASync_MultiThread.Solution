b4ebfd79e3b46a2993474ac20538268a4825b914
LillianLin <a888gogogo@yahoo.com.tw>
2021-03-15 14:42:17 +0800

    add AsyncExec project

---
+++ AsyncExec/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AsyncExec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- WhenAnyTest/UnitTest1.cs
+++ WhenAnyTest/UnitTest1.cs
@@ -1,7 +1,35 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace WhenAnyTest
 {
+    public class Solution
+    {
+        /// <summary>
+        /// 若指定時限內還沒取得記錄，就傳回null
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public async Task<string> DownloadStringWithTimeount(string url, int timeout)
+        {
+            using (var client = new HttpClient())
+            {
+                var downloadTask = client.GetStringAsync(url);
+                var timeoutTask = Task.Delay(timeout);
+
+                var completedTask = await Task.WhenAny(downloadTask, timeoutTask);
+                if (completedTask == timeoutTask)
+                {
+                    return null;
+                }
+
+                return await downloadTask;
+            }
+        }
+    }
+
     public class Tests
     {
         [SetUp]
@@ -10,9 +38,27 @@ namespace WhenAnyTest
         }
 
         [Test]
-        public void Test1()
+        public async Task Demo_3秒內沒取回網頁就傳回null_成功()
         {
-            Assert.Pass();
+            string url = "https://www.google.com/";
+            int timeout = 3000;
+            var sut = new Solution();
+            string result = await sut.DownloadStringWithTimeount(url, timeout);
+            
+            Assert.IsNotNull(result);
+            Console.WriteLine($"total char count :{result.Length}");
+        }
+        
+        [Test]
+        public async Task Demo_0_01秒內沒取回網頁就傳回null_傳回null()
+        {
+            string url = "https://www.google.com/";
+            int timeout = 1;
+            var sut = new Solution();
+            string result = await sut.DownloadStringWithTimeount(url, timeout);
+            
+            Assert.IsNull(result);
+            
         }
     }
 }
