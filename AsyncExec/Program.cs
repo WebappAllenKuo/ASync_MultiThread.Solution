ccc77637a6d66d607180b3cfcbbacdda196df26f
LillianLin <a888gogogo@yahoo.com.tw>
2021-03-15 14:53:59 +0800

    改寫非同步，由於非同步尚未完成，main 就結束了，所以background thread自動也結束

--- AsyncExec/Program.cs
+++ AsyncExec/Program.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncExec
 {
@@ -9,12 +10,12 @@ namespace AsyncExec
     {
         static void Main(string[] args)
         {
-            Log("Main start.");
+            Log("Main start.",1);
 
             string url = "http://www.google.com/";
-            DownloadString(url);
+            DownloadStringAsync(url); // 由於非同步尚未完成，main 就結束了，所以background thread自動也結束
             
-            Log("Main end.");
+            Log("Main end.", 2);
         }
 
         static void DownloadString(string url)
@@ -25,6 +26,16 @@ namespace AsyncExec
                 Log($"DownloadString/content has {content.Length} chars.");
             }
         }
+        
+        static async Task DownloadStringAsync(string url)
+        {
+            using (var client =new HttpClient())
+            {
+                string content =await client.GetStringAsync(url);
+                Log($"DownloadString/content has {content.Length} chars.",3);
+            }
+        }
+        
         static void Log( string msg,int? seqNum=null)
         {
             string numTtext = seqNum.HasValue ? seqNum.Value.ToString() : string.Empty;
